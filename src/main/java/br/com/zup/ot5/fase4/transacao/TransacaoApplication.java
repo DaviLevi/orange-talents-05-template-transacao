ae6b8e2301a3481b59142c5322f3896f50366027
DaviLevi <davide.sgalambro@gmail.com>
2021-06-11 10:38:15 -0300

    0.0 Setup Projeto

---
+++ src/main/java/br/com/zup/ot5/fase4/transacao/TransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ot5.fase4.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/ot5/fase4/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.ot5.fase4.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
